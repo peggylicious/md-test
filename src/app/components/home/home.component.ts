78620b5ee90475bc8ad6c38506b3ed1a250a0691
peggylicious <p.asq@live.com>
2024-08-28 16:30:31 +0100

    #1 create a post form

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,12 +1,17 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import { PostFormComponent } from '../../shared/post-form/post-form.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [NgIf, PostFormComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  IS_SHOW_FORM: boolean = false;
+  showForm(){
+    return this.IS_SHOW_FORM = !this.IS_SHOW_FORM;
+  }
 }
