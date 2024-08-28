35abc72e3098c112c9c362b2e549d5072539df26
peggylicious <p.asq@live.com>
2024-08-28 17:05:39 +0100

    #1 update post form with styles

--- src/app/shared/post-form/post-form.component.ts
+++ src/app/shared/post-form/post-form.component.ts
@@ -1,12 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'post-form',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule, ],
   templateUrl: './post-form.component.html',
   styleUrl: './post-form.component.css'
 })
 export class PostFormComponent {
+  fb = inject(FormBuilder)
 
+  postForm: FormGroup  = this.fb.group({
+    id: [''],
+    userId: [''],
+    title: [''],
+    body: ['']
+  })
+
+  initializeForm(){
+    return this.postForm = this.fb.group({
+      id: [''],
+      userId: [''],
+      title: [''],
+      body: ['']
+    })
+  }
+  submit(){
+    console.log(this.postForm.value)
+  }
 }
